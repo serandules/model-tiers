7b673000a653976ea2595f0a7e6d663464181a0d
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-05-10 10:13:23 +0530

    updating

---
+++ model.js
@@ -0,0 +1,38 @@
+var log = require('logger')('model-tiers');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var mongins = require('mongins');
+
+var validators = require('validators');
+
+var types = validators.types;
+var values = validators.values;
+
+var tier = Schema({
+  name: {
+    type: String,
+    required: true,
+    validator: types.string({
+      length: 20
+    })
+  },
+  description: {
+    type: String,
+    validator: types.string({
+      length: 1000
+    })
+  },
+  limits: {
+    type: Object,
+    required: true,
+    server: true
+  }
+}, {collection: 'tiers'});
+
+tier.plugin(mongins);
+tier.plugin(mongins.user);
+tier.plugin(mongins.createdAt);
+tier.plugin(mongins.updatedAt);
+
+module.exports = mongoose.model('tiers', tier);
